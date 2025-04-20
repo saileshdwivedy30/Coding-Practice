f47252366f4469056e4c5331f07fb100e8dc6f03
saileshdwivedy <saileshdwivedy3003@gmail.com>
2025-04-20 11:03:10 -0600

    merge intervals-conflicting appointments

---
+++ LeetCode/Merge Intervals/Conflicting Appointments.py
@@ -0,0 +1,32 @@
+
+from typing import List
+
+class Solution:
+    def canAttendAppointments(self, intervals: List[List[int]]) -> bool:
+        # Your code here
+        if len(intervals)<=1:
+            return True
+
+        intervals.sort(key=lambda x: x[0])
+
+        for i in range(1, len(intervals)):
+            current_start=intervals[i][0]
+            prev_end=intervals[i-1][1]
+            if current_start <= prev_end:
+                return False
+        return True
+
+# Example test cases
+if __name__ == "__main__":
+    sol = Solution()
+
+    # Test Case 1
+    assert sol.canAttendAppointments([[1,4], [2,5], [7,9]]) == False, "Test Case 1 Failed"
+
+    # Test Case 2
+    assert sol.canAttendAppointments([[6,7], [2,4], [8,12]]) == True, "Test Case 2 Failed"
+
+    # Test Case 3
+    assert sol.canAttendAppointments([[4,5], [2,3], [3,6]]) == False, "Test Case 3 Failed"
+
+    print("All test cases passed ✅")
